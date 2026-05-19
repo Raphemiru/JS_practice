c8de813f8fd12bf3884b9955f7b4e29a1e08614b
Gab <fga.080272@gmail.com>
2026-05-20 00:10:07 +0800

    Access the DOM with const included

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("HELLO WORLD!");
+// API
+const apiKey = config.apiKey;
+
+// SEARCHBAR/BUTTON
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+// WEATHER MAIN
+const weatherMainIcon = document.getElementById("weather-main-icon");
+const temperature = document.getElementById("temperature");
+const city = document.getElementById("city");
+
+// WEATHER DETAILS
+const humidityLevel = document.getElementById("humidity-level");
+const windSpeed = document.getElementById("wind-speed");
