e70711a634fde50a134aee20a2c19ee2f8b9f0de
Gab <fga.080272@gmail.com>
2026-05-20 16:20:45 +0800

    FInal Commit: Weather App #9293492384

--- script.js
+++ script.js
@@ -16,17 +16,40 @@ const windSpeed = document.getElementById("wind-speed");
 
 const fetchWeather = async () => {
   try {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
     const response = await fetch(apiUrl);
     const weatherData = await response.json();
-    console.log(weatherData);
-    // the other code
+    const mistConditions = [
+      "Mist",
+      "Smoke",
+      "Haze",
+      "Dust",
+      "Fog",
+      "Sand",
+      "Ash",
+    ];
+    const windConditions = ["Squall", "Tornado"];
+
+    const condition = weatherData.weather[0].main;
+
+    if (mistConditions.includes(condition)) {
+      weatherMainIcon.src = "images/mist.png";
+    } else if (windConditions.includes(condition)) {
+      weatherMainIcon.src = "images/wind.png";
+    } else {
+      weatherMainIcon.src = `images/${condition.toLowerCase()}.png`;
+    }
+
+    temperature.textContent = weatherData.main.temp;
+    city.textContent = weatherData.name;
+    humidityLevel.textContent = weatherData.main.humidity;
+    windSpeed.textContent = weatherData.wind.speed;
   } catch (error) {
     console.log(`Error: ${error}`);
   }
 };
 
-searchButton.addEventListener("click", () => {
+searchButton.addEventListener("click", (event) => {
   console.log("button clicked");
   fetchWeather();
   event.preventDefault();
